1c17dc172d25a026f7b442bcd154c5ae8caa28f2
DanendraF <email.lenovoipslim5i@gmail.com>
2026-06-12 04:02:21 +0700

    fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart({
-      server: { entry: "server" },
+      server: { entry: "server", preset: "vercel" },
     }),
+    nitro(),
     react(),
     tailwindcss(),
     tsconfigPaths(),
